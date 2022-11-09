e7875bfb30380a9224325a3cedde13365f48948d
stiv39 <jarsinsky.norbert@gmail.com>
2022-11-09 20:34:44 +0100

    init app

---
+++ vite-portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <main>
+        <section className=" bg-blue-900 min-h-screen">
+          <h1>Hello World</h1>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
